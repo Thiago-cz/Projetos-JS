43c4bbd662f055ecd69927a7d1f7fdcec441bf52
Thiago-cz <thiagoocvlhdesouza@gmail.com>
2023-02-07 09:51:33 -0300

    upload

--- Calculadora/script.js
+++ Calculadora/script.js
@@ -1,76 +1,131 @@
 function insert(num) {
-	var text = document.querySelector("#result").textContent
-	if(text.length <= 14) {
+	let result = document.querySelector("#result").innerHTML
+	let previus = document.querySelector("#previus").innerHTML
+
+	if (checkOperation() && result.length < 11) {
+		document.querySelector("#previus").innerHTML += num
+
+		if (checkSimbol(previus.charAt(previus.length - 1))) {
+			document.querySelector("#result").innerHTML = ""
+			console.log(previus.charAt(previus.length - 1))
+		}
+		document.querySelector("#result").innerHTML += num
+	} else {
 		document.querySelector("#result").innerHTML += num
 	}
 }
-function simbol(num) {
-	let n = document.querySelector("#result").innerHTML
-	n = n.charAt(n.length - 1)
 
-	if (n != "") {
-		switch (n) {
-			case "+": break
-			case "-": break
-			case "*": break
-			case "/": break
-			case ".": break
-			default: document.querySelector("#result").innerHTML += num
-		}
+function checkOperation() {
+	var previus = document.querySelector("#previus").innerHTML
+
+	if (previus.includes("-") || previus.includes("+") || previus.includes("*") || previus.includes("/")) {
+		return true
 	}
+	return false
+}
+function checkSimbol(simbol) {
+	if (simbol == "+" || simbol == "-" || simbol == "*" || simbol == "/") {
+		return true
+	}
+	return false
+}
+
+function simbol(simbol) {
+	let result = document.querySelector("#result").innerHTML
+	result += simbol
+	document.querySelector("#previus").innerHTML = result
+}
+function virgula() {
+	document.querySelector("#result").innerHTML += "."
 }
 
 function clean() {
 	document.getElementById("result").innerHTML = ""
+	document.getElementById("previus").innerHTML = ""
 }
 
 function clearLast() {
-	var result = document.getElementById("result").innerHTML
+	let result = document.querySelector("#result").innerHTML
+	let previus = document.querySelector("#previus").innerHTML
 	document.getElementById("result").innerHTML = result.substring(0, result.length - 1)
+	document.getElementById("previus").innerHTML = previus.substring(0, previus.length - 1)
+}
+let lastResult = 0
+
+function soma(num1, num2,) {
+	let n1 = parseFloat(num1), n2 = parseFloat(num2)
+	let result = 0
+
+	if (lastResult == 0) {
+		result = n1 + n2
+		return result
+	}
+	result = lastResult + n1
+	return result
 }
 
-function finalResult() {
-	var content = document.querySelector("#result").innerHTML
-	var n1 = new Array, simbols = new Array, n2 = new Array
-	var control1 = false
-	var control2 = true
+function subtracao(num1, num2) {
+	let n1 = parseFloat(num1), n2 = parseFloat(num2), result = 0
 
-	if (content) {
+	if (lastResult == 0) {
+		result = n1 - n2
+		return result
+	}
+	result = lastResult - n1
+	return result
+}
 
-		for (i = 0; i < content.length; i++) {
-			var char = content.charAt(i)
+function multiplicacao(num1, num2) {
+	let n1 = parseFloat(num1), n2 = parseFloat(num2), result
 
-			if (control2 & char != "+" & "-" & "*" & "/") {
-				n1.push(char)
-				control1 = false
-			} else {
-				simbols.push(char)
-				control1 = true
-				control2 = false
-			}
-			if (control1 & char != "+" & "-" & "*" & "/") {
-				n2.push(char)
-			} else {
-				control2 = true
-			}
-		}
+	if (lastResult == 0) {
+		result = n1 * n2
+		result = parseFloat(result.toFixed(9))
+		return result
+	}
+	result = lastResult * n1
+	return parseFloat(result.toFixed(9))
+}
+function divisao(num1, num2) {
+	let n1 = parseFloat(num1), n2 = parseFloat(num2), result
 
-		var result = 0, first = true
+	if (lastResult == 0) {
+		result = n1 / n2
+		return parseFloat(result.toFixed(9))
+	}
+	result = lastResult / n1
+	return result.toFixed(9)
+}
 
-		for (var i = 0; i < simbols.length - 1; i++) {
-			var x = simbols
+function result() {
+	let previus = document.querySelector("#previus").innerHTML
+	let num1 = "", num2 = "", key = true, simbol
 
-			if (first) {
-				switch (x) {
-					case "/":
-						result = n1[i] / n2[i]
+	for (var iterator of previus) {
 
-					case "*": result = n1[i] * n2[i]
-				}
-			} else {
-
-			}
+		if (!checkSimbol(iterator) && key) {
+			num1 += iterator
+		} else if (!key) {
+			num2 += iterator
+		} else {
+			key = false
+			simbol = iterator
 		}
 	}
 
+	switch (simbol) {
+		case '+':
+			document.querySelector("#result").innerHTML = soma(num1, num2)
+			break
+		case '-':
+			document.querySelector("#result").innerHTML = subtracao(num1, num2)
+			break
+		case '*':
+			document.querySelector("#result").innerHTML = multiplicacao(num1, num2)
+			break
+		case '/':
+			document.querySelector("#result").innerHTML = divisao(num1, num2)
+			break
+	}
+	document.querySelector("#previus").innerHTML = ""
 }
